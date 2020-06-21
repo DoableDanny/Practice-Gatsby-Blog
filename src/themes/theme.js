29f6bc29cc3b55e5d39c6a798e95493f69f20b5a
Danny Adams <dantheado@gmail.com>
2020-06-21 17:03:32 +0100

    Add theme provider

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -18,8 +18,8 @@ export default {
   },
   spacings: {
     xxSmall: ".25rem",
-    xxSmall: ".5rem",
-    Small: "1rem",
+    xSmall: ".5rem",
+    small: "1rem",
     medium: "2rem",
     large: "3rem",
     xLarge: "4rem",
