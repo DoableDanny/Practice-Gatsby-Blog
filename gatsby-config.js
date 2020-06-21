7904bc1826d46215b830e0e4810ef4c89ec8104a
Danny Adams <dantheado@gmail.com>
2020-06-21 15:59:57 +0100

    added initial posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,21 +11,21 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
-        path: "${__dirname}/src/pages",
+        path: "src/pages",
       },
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "posts",
-        path: "${__dirname}/src/posts",
+        path: "src/posts",
       },
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "${__dirname}/src/images",
+        path: "src/images",
       },
     },
     `gatsby-plugin-sharp`,
