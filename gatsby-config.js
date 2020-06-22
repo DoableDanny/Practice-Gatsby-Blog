36bda5fcf6c3ee7da752629ecc81952d7b516402
Danny Adams <dantheado@gmail.com>
2020-06-22 17:18:19 +0100

    Increased maxWidth and added quality

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,8 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 1200,
+              maxWidth: 1500,
+              quality: 85,
             },
           },
         ],
