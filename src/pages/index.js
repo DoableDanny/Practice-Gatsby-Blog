a5e4c1cb24281c0d410ba277ef3ded0f109be726
Danny Adams <dantheado@gmail.com>
2020-06-23 17:03:21 +0100

    Created content card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          data="March 22, 2020"
+          title="lorem ipsum dolor sit amet"
+          excerpt="Lorem ipsum has become the industry standard for design mockups and prototypes. By adding a little bit of Latin to a mockup, you’re able to show clients a more complete version of your design without actually having to invest time and effort drafting copy."
+          slug="/test"
+        />
+      </Content>
     </Container>
   )
 }
